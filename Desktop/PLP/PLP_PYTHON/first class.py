83400a84c31b71c7a0876e49badba1cca1529306
NUMFOR LINDA BIH <lilexbih@yahoo.com>
2025-08-18 02:46:13 +0300

    wk3 python asiign

---
+++ Desktop/PLP/PLP_PYTHON/first class.py
@@ -0,0 +1,23 @@
+basket = 10.5
+bucket ="water"
+
+print("This class is crazy")
+a,b,c = 5, 7, "Hello world"
+print(a) # prints 5
+print(b) # prints 5
+print(c) # prints Hello World
+#dic
+
+Maseret = {"name": "Maseret", "age": 25, "city": "Addis Ababa"}
+
+}
+
+print(Maseret["name"])  # prints Maseret
+
+
+print(Maseret["age"])   # prints 25
+
+#Boolean
+
+last_born =True#set data type
+my_basket = {"bananas": 10.5, "bucket": "water"}
